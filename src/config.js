95846302ea43e2b88e70fef0085d0a16344a3476
Fa!ko Krause <falko@webpgr.com>
2017-11-17 12:14:35 +0100

    switch files and increase number of data to pull

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	// date2mIdPath: `${__dirname}/../data/date2metaDataId.json`
-	date2mIdPath: `${__dirname}/../data/date2metaDataId.example.json`,
+	date2mIdPath: `${__dirname}/../data/date2metaDataId.json`,
 	dataDir: `${__dirname}/../data`,
 	twitterCredentials: require('../keys.js'),
 	tweetsPath: `${__dirname}/../data/tweets.json`,

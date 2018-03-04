43270c02f9159be012e30c7296ec43a2954552d5
Fa!ko Krause <falko@webpgr.com>
2018-03-05 00:31:04 +0100

    simplify code

--- server.js
+++ server.js
@@ -17,21 +17,6 @@ tweetsDb.defaults([]).write();
 const userDb = low(new FileSync('data/users.json'));
 userDb.defaults({}).write();
 
-
-function authenticateSession(username, sessionToken, cb, successCallback) {
-	try {
-		const user = userDb.read().get(username).value();
-		if (!user) return cb && cb({ error: 'Username or token wrong.' });
-		const result = pwd.verifySync(Buffer.from(sessionToken), Buffer.from(user.sessionTokenHash.data));
-		if (result === securePassword.INVALID) return cb && cb({ error: 'Username or token wrong.' });
-		if (result === securePassword.VALID) {
-			successCallback();
-		}
-	} catch (error) {
-		cb && cb({ error: `Something went wrong. ${error}` });
-	}
-}
-
 io.on('connection', function(socket) {
 	// Login with username: if sucessfull return a session token and
 	// store the secure hash token in the user db.
@@ -39,18 +24,14 @@ io.on('connection', function(socket) {
 		const user = userDb.read().get(username).value();
 		if (!user) return cb({ error: 'Username or password wrong.' });
 		if (!(user.passwordHash)) return cb && cb({ error: 'Please contact an admin and ask for a new registration link.' });
-		let result;
-		try {
-			result = pwd.verifySync(
-				Buffer.from(password),
-				Buffer.from(user.passwordHash.data)
-			);
-		} catch (error) {
-			return cb && cb({ error: `Crashed: ${error}` });
-		}
+		const result = pwd.verifySync(
+			Buffer.from(password),
+			Buffer.from(user.passwordHash.data)
+		);
 		if (result === securePassword.INVALID) return cb({ error: 'Username or password wrong.' });
 		if (result === securePassword.VALID) {
 			socket.authenticated = true;
+			socket.username = username;
 			const sessionToken = uuidv4();
 			const sessionTokenHash = pwd.hashSync(Buffer.from(sessionToken));
 			userDb.set(`${username}.sessionTokenHash`, sessionTokenHash).write();
@@ -60,23 +41,25 @@ io.on('connection', function(socket) {
 
 	// Login with session token: check if the user has a matching token in the db.
 	socket.on('login with sessionToken', ({ username, sessionToken }, cb) => {
-		authenticateSession(username, sessionToken, cb, () => {
+		const user = userDb.read().get(username).value();
+		if (!user) return cb && cb({ error: 'Username or token wrong.' });
+		const result = pwd.verifySync(Buffer.from(sessionToken), Buffer.from(user.sessionTokenHash.data));
+		if (result === securePassword.INVALID) return cb && cb({ error: 'Username or token wrong.' });
+		if (result === securePassword.VALID) {
 			socket.authenticated = true;
+			socket.username = username;
 			cb && cb({ success: true, tweets: tweetsDb.getState() });
-		});
+		}
 	});
 
 	// Logout: Remove authenticated flag and delete the session token.
-	socket.on('logout', ({ username, sessionToken }, cb) => {
-		authenticateSession(username, sessionToken, cb, () => {
-			socket.authenticated = false;
-			userDb.set(`${username}.sessionTokenHash`, undefined).write();
-			cb && cb({ success: true });
-		});
-	});
+	socket.on('logout', checkAuthenticated(socket, (a, cb) => {
+		socket.authenticated = false;
+		userDb.unset(`${session.username}.sessionTokenHash`).write();
+		cb && cb({ success: true });
+	}));
 
-	// Register: This must happen in a secure namespace or everybody can registerC
-	// Create a new user entry and calculate a secure password hash.
+	// Register: Create a new user entry and calculate a secure password hash.
 	socket.on('register', ({ username, password, registrationToken }, cb) => {
 		console.log("registrationToken", registrationToken);
 		const registrationTokenBuffer = userDb.read().get(`${username}.registrationTokenBuffer.data`).value();
@@ -94,6 +77,7 @@ io.on('connection', function(socket) {
 				.set(username, { passwordHash })
 				.unset(`${username}.registrationTokenBuffer`)
 				.write();
+			// TODO add broadcast here
 			cb && cb({ success: `Registered new user <b>${username}</b>.` });
 		}
 	});
@@ -105,6 +89,7 @@ io.on('connection', function(socket) {
 			const registrationToken = uuidv4();
 			const registrationTokenBuffer = pwd.hashSync(Buffer.from(registrationToken));
 			userDb.set(username, { registrationTokenBuffer }).write();
+			// TODO add broadcast here
 			cb && cb({ success: 'Edit success.',user: { username, registrationToken, hasRegistrationToken: true } });
 		})
 	);
@@ -113,6 +98,7 @@ io.on('connection', function(socket) {
 		'delete user',
 		checkAuthenticated(socket, ({ username }, cb) => {
 			userDb.unset(username).write();
+			// TODO add broadcast here
 			cb && cb({ success: 'Delete success.'});
 		})
 	);
@@ -124,6 +110,7 @@ io.on('connection', function(socket) {
 				.nth(index)
 				.assign(data)
 				.write();
+			// TODO add broadcast here
 			cb && cb({ success: 'Edit success.' });
 		})
 	);
